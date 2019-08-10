2073d9ee2284f40858b19008cdc9af1e1c43015c
Ahyun Jeong <44775006+jeongahyun@users.noreply.github.com>
2019-08-10 23:30:58 +0900

    Update urls.py

--- first/urls.py
+++ first/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     path('signout/',views.signout, name="signout"),
     path('confirm/', views.confirm_email, name="confirm_email"),
     path('email_sent/', views.email_sent, name="email_sent"),
-<<<<<<< HEAD
-    path('accounts/',include('django.contrib.auth.urls')),
-    path('bestphoto/',views.bestphoto,name="bestphoto"),
-=======
     path('bestphoto/',views.bestphoto, name="bestphoto"),
->>>>>>> 82f4b21d96c5f5d204b295379410cdd20b705d30
     path('recommend/',views.recommend,name="recommend"),
 ]
