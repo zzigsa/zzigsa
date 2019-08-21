1098f459297cfa82ced8e534c346a03dcd990411
CitrusSoda <dndhkaus1004@naver.com>
2019-08-21 19:23:32 +0900

    Add : productedit

--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def product(request):
+    return render(request, 'product.html')
+
+def productedit(request):
+    return render(request, 'productedit.html')
